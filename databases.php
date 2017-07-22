5ab43ff3c4b86f2f7fc27a9da59356714864b841
8hubham <8hubham@gmail.com>
2017-07-22 20:29:20 +0530

    colors to status column

--- databases.php
+++ databases.php
@@ -35,15 +35,25 @@ table {
 }
 
 td, th {
-    border: 2px solid black;
+    border: 0px solid black;
     text-align: center;
     vertical-align: center;
-    padding: 8px;
+    padding: 12px;
+    width: 50%;
 
 }
 
 tr:nth-child(even) {
-    background-color: #dddddd;
+    background-color: #f5f5f5;
+}
+
+.on {
+	background-color: #00ffb4;
+	font-size: 12;
+}
+.off {
+	background-color: #ffa030;
+	font-size: 12;
 }
 </style>
 </head>
@@ -52,16 +62,19 @@ tr:nth-child(even) {
 <?php
 	$avg_time = [];
 	$ip_address = [];
+	$status = [];
 	$i = 0;
 	while($tempstring = mysqli_fetch_assoc($result))
 	{
 		$avg_time[$i]=$tempstring["avg_time"];
 		$ip_address[$i]=$tempstring["ip_address"];
+		$status[$i]=$tempstring["status"];
 		$i++;
 	}
 
 ?>
 <center>
+<h2><b><u>Data Monitoring</u></b></h2>
 <table>
   <tr>
     <th>IP Address</th>
@@ -71,52 +84,112 @@ tr:nth-child(even) {
   <tr>
     <td><?php echo("$ip_address[0]")?></td>
     <td><?php echo("$avg_time[0]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[0] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[1]")?></td>
     <td><?php echo("$avg_time[1]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[1] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[2]")?></td>
     <td><?php echo("$avg_time[2]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[2] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[3]")?></td>
     <td><?php echo("$avg_time[3]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[3] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[4]")?></td>
     <td><?php echo("$avg_time[4]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[4] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[5]")?></td>
     <td><?php echo("$avg_time[5]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[5] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[6]")?></td>
     <td><?php echo("$avg_time[6]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[6] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[7]")?></td>
     <td><?php echo("$avg_time[7]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[7] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[8]")?></td>
     <td><?php echo("$avg_time[8]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[8] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
   <tr>
     <td><?php echo("$ip_address[9]")?></td>
     <td><?php echo("$avg_time[9]")?></td>
-    <td>ON</td>
+    <td>
+    <?php if($status[9] == 1) { ?>
+    <p class="on">ON</p>
+    <?php }else{?>
+    <p class="off">OFF</p>
+    <?php } ?>
+    </td>
   </tr>
 </table>
 </center>
